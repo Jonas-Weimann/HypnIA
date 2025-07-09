04b8533753fbed4516f7c6579ef2e7093a247a99
Jonas <jonas.weimann04@gmail.com>
2025-07-09 02:45:48 -0300

    feat: Agrega el middleware de interpretación a la ruta de sueños y actualiza lógica de endpoints de creación y actualización de sueños.

--- backend/src/rutas/suenos-ruta.js
+++ backend/src/rutas/suenos-ruta.js
@@ -1,13 +1,22 @@
-const { Router } = require('express');
-const { getAllSuenos, getSuenosPublicos, getSuenoById, createSueno, updateSueno, deleteSueno } = require('../controladores/suenos-controlador.js');
+const { Router } = require("express");
+const {
+  getAllSuenos,
+  getSuenosPublicos,
+  getSuenoById,
+  createSueno,
+  updateSueno,
+  deleteSueno,
+} = require("../controladores/suenos-controlador.js");
+const { autenticarUsuario } = require("../middlewares/autenticacion.js");
+const { interpretarSueno } = require("../middlewares/interpretarSueno.js");
 
 const router = Router();
 
-router.get('/', getAllSuenos);
-router.get('/publicos', getSuenosPublicos);
-router.get('/:sid', getSuenoById);
-router.post('/', createSueno);
-router.put('/:sid', updateSueno);
-router.delete('/:sid', deleteSueno);
+router.get("/", getAllSuenos);
+router.get("/publicos", getSuenosPublicos);
+router.get("/:sid", getSuenoById);
+router.post("/", /*autenticarUsuario,*/ interpretarSueno, createSueno);
+router.put("/:sid", interpretarSueno, updateSueno);
+router.delete("/:sid", deleteSueno);
 
 module.exports = router;
