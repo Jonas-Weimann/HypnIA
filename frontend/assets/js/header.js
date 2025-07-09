0220fdfb6b9dccd3b1539149f04babff100009e3
angelesazulninaja <aninaja@fi.uba.ar>
2025-07-09 16:09:49 +0000

    feat: agrega página About al frontend con su HTML, CSS y ajustes en inicio.html para navegación

---
+++ frontend/assets/js/header.js
@@ -0,0 +1,7 @@
+const header = document.getElementById('header');
+fetch("components/header.html")
+    .then(res => res.text())
+    .then(html => {
+      header.innerHTML = html;
+    })
+    .catch(err => console.error('Error al cargar el header:', err));
