35eede631a78a5343e201b2cd0e15edab556e35c
Jonas <jonas.weimann04@gmail.com>
2025-07-14 01:35:28 -0300

    fix: Agrega límites a los inputs de registro y setea info de usuario en local storage

--- frontend/assets/js/header.js
+++ frontend/assets/js/header.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		if (logout) {
 			logout.addEventListener("click", e => {
 			e.preventDefault();
-			localStorage.removeItem("token");
+			localStorage.clear()
 			window.location.reload();
 			});
 		}
