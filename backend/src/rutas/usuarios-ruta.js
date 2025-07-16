295769b206da0efaed358be60671c15d8cf8d7db
Jonas <jonas.weimann04@gmail.com>
2025-07-16 01:07:45 -0300

    fix: Arregla error fatal de rutas de usuarios get superpuestas

--- backend/src/rutas/usuarios-ruta.js
+++ backend/src/rutas/usuarios-ruta.js
@@ -21,12 +21,12 @@ import {
 const router = Router();
 
 router.get("/", autenticarAdmin, getAllUsuarios);
+router.get("/activo", esUsuarioActivo);
 router.get("/:uid", autenticarAdmin, getUsuarioById);
 router.get("/:uid/suenos", autenticarUsuario, getSuenosByUsuario);
 router.get("/:uid/suenos-publicos", getSuenosPublicosByUsuario);
 router.post("/registrar", registrarUsuario);
 router.post("/iniciar-sesion", iniciarSesion);
-router.get("/activo", esUsuarioActivo);
 router.put("/cambiar-contrasena", cambiarContrasena);
 router.put("/cambiar-foto", autenticarUsuario, cambiarFoto);
 router.put("/cambiar-nombre", autenticarUsuario, cambiarNombre);
