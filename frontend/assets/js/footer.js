87fa5388708a517de01c6953b82879fa6ec57b8a
Jonas Weimann <jonas.weimann04@gmail.com>
2025-07-17 04:07:05 -0300

    merge: Integra la página para administrar las entidades del sitio (#46)
    * feat: Agrega estructura inicial para pagina de admins
    
    * style: Agrega estilos iniciales para pagina de admins
    
    * feat: Agrega eliminación de cartas
    
    * fix: Cambia nombres de elementos para evitar problemas de redeclaración
    
    * feat: Agrega eliminación de emociones
    
    * feat: Agrega eliminación de usuarios
    
    * feat: Agrega formulario emergente para edicion de cartas
    
    * feat: Agrega formulario emergente para edición de emociones
    
    * fix: Corrige error de renderizado de cartas despues de editadas
    
    * fix: Corrige error de renderizado de emociones despues de editadas
    
    * feat: Agrega formulario emergente para edición de usuarios
    
    * feat: Agrega formulario emergente para creación de cartas
    
    * feat: Agrega formulario emergente para creación de emociones e imagenes faltantes
    
    * feat: Agrega redireccion a register para crear usuarios
    
    * feat: Agrega boton de redireccion a admin-page en header y redireccion en footer

--- frontend/assets/js/footer.js
+++ frontend/assets/js/footer.js
@@ -1,34 +1,34 @@
 document.addEventListener("DOMContentLoaded", async () => {
-	const footer = document.getElementById("footer");
-	const sesionActiva = await verificarSesion();
-	if(!footer) return;
+  const footer = document.getElementById("footer");
+  const sesionActiva = await verificarSesion();
+  if (!footer) return;
 
-	fetch("components/footer.html")
-	.then(res => res.text())
-	.then(html => { 
-		footer.innerHTML = html;
-		copiarEmail();
-		modificarFooter(sesionActiva, footer);
-	})
-	.catch(err => console.error("Error al cargar el header:", err));
+  fetch("components/footer.html")
+    .then((res) => res.text())
+    .then((html) => {
+      footer.innerHTML = html;
+      copiarEmail();
+      modificarFooter(sesionActiva, footer);
+    })
+    .catch((err) => console.error("Error al cargar el header:", err));
 });
-
 
 const copiarEmail = () => {
   const linksEmails = document.querySelectorAll(".copiar-email");
 
-  linksEmails.forEach(linkEmail => {
-		linkEmail.addEventListener("click", (e) => {
-		e.preventDefault();
-		const email = linkEmail.dataset.email;
-		navigator.clipboard.writeText(email)
-			.then(() => {
-				mostrarMensajeCopiado();
-			})
-			.catch(error => {
-				console.error("Error al copiar el correo:", error);
-			});
-		});
+  linksEmails.forEach((linkEmail) => {
+    linkEmail.addEventListener("click", (e) => {
+      e.preventDefault();
+      const email = linkEmail.dataset.email;
+      navigator.clipboard
+        .writeText(email)
+        .then(() => {
+          mostrarMensajeCopiado();
+        })
+        .catch((error) => {
+          console.error("Error al copiar el correo:", error);
+        });
+    });
   });
 };
 
@@ -46,25 +46,25 @@ const mostrarMensajeCopiado = () => {
 };
 
 const modificarFooter = (sesionActiva, footer) => {
-	const token = localStorage.getItem("token");
-	if (!token || !sesionActiva) return;
+  const token = localStorage.getItem("token");
+  if (!token || !sesionActiva) return;
 
-	const contenedor = footer.querySelector(".login-register-contenedor");
-	if (!contenedor) return;
+  const contenedor = footer.querySelector(".login-register-contenedor");
+  if (!contenedor) return;
 
-	contenedor.innerHTML = `
+  contenedor.innerHTML = `
 		<li><a href="inicio.html">Inicio</a></li>
 		<li><a href="profile.html">Perfil</a></li>
-		<li><a href="#" id="cerrar-sesion-footer">Cerrar sesión</a></li>
+		<li><a href="inicio.html" id="cerrar-sesion-footer">Cerrar sesión</a></li>
 		
 	`;
 
-	const logout = footer.querySelector("#cerrar-sesion-footer");
-	if (logout) {
-		logout.addEventListener("click", e => {
-		e.preventDefault();
-		localStorage.clear();
-		window.location.reload();
-		});
-	}
+  const logout = footer.querySelector("#cerrar-sesion-footer");
+  if (logout) {
+    logout.addEventListener("click", (e) => {
+      e.preventDefault();
+      localStorage.clear();
+      window.location.reload();
+    });
+  }
 };
---
+++ frontend/assets/js/usuarios-admin.js
@@ -0,0 +1,187 @@
+const usuariosURL = "http://localhost:3000/api/usuarios";
+const usuariosBusqueda = document.getElementById("usuarios-busqueda");
+const contenedorUsuarios = document.getElementById("usuarios-contenedor");
+const btnGestionarUsuarios = document.querySelector(".gestionar-usuarios");
+const popupUsuarios = document.querySelector(".popup-usuarios");
+const popupEdicionUsuarios = document.querySelector(".popup-edicion-usuarios");
+btnGestionarUsuarios.addEventListener("click", () => {
+  contenedorUsuarios.classList.toggle("activo");
+  contenedorEmociones.classList.remove("activo");
+  contenedor.classList.remove("activo");
+});
+let usuarios = [];
+
+const obtenerUsuarios = async () => {
+  try {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+
+    const respuesta = await fetch(usuariosURL, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await respuesta.json();
+    usuarios = data;
+    mostrarUsuarios(usuarios);
+  } catch (error) {
+    console.error("Error obteniendo usuarios:", error);
+  }
+};
+
+const mostrarUsuarios = (usuariosAMostrar) => {
+  contenedorUsuarios.innerHTML = "";
+  const token = localStorage.getItem("token");
+  if (!token) return;
+
+  if (usuariosAMostrar.length === 0) {
+    contenedorUsuarios.innerHTML = "<p>No se encontraron usuarios.</p>";
+    return;
+  }
+
+  usuariosAMostrar.forEach((usuario) => {
+    const usuarioDiv = document.createElement("div");
+    usuarioDiv.classList.add("usuario-card");
+    usuarioDiv.innerHTML = `
+			<div class="usuario">
+      <button class="eliminar-usuario" data-id="${
+        usuario.id_usuario
+      }">❌</button>
+			  <button class="editar-usuario" data-id="${usuario.id_usuario}">🖊️</button>
+        <img class="foto-perfil" src='./assets/images/profile-img/${
+          usuario.foto_perfil
+        }'/>
+				<div class="nombre-usuario"><h2>${usuario.nombre.toUpperCase()}</h2></div></div>
+				<div class="informacion-usuario">
+					<p>ID: ${usuario.id_usuario}</p>
+					<p>EMAIL: ${usuario.email}</p>
+					<p>REGISTRO: ${new Date(usuario.fecha_registro).toLocaleDateString()}</p>
+				</div>
+		`;
+    contenedorUsuarios.appendChild(usuarioDiv);
+    const btnEliminar = usuarioDiv.querySelector(".eliminar-usuario");
+    btnEliminar.addEventListener("click", () => {
+      popupUsuarios.classList.add("activo");
+      popupUsuarios.innerHTML = `
+				<h2>¿Eliminar Usuario?</h2>
+				<br/>
+				<span>Esta acción no puede ser revertida ni desde el más allá.</span>
+				<div class="confirmaciones">
+					<button class="volver">Volver</button>
+					<button class="confirmar-eliminar" data-id="${usuario.id_usuario}">Eliminar</button>
+				</div>
+			`;
+    });
+    const btnEditar = usuarioDiv.querySelector(".editar-usuario");
+    btnEditar.addEventListener("click", () => {
+      popupEdicionUsuarios.classList.add("activo");
+      popupEdicionUsuarios.innerHTML = `
+        <h2>Editar Usuario</h2>
+          <form id="form-editar-usuario">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" value="${usuario.nombre}" required />
+  
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" value="${usuario.email}" required />
+  
+            <label for="foto_perfil">Foto de perfil:</label>
+            <input type="text" id="foto_perfil" name="foto_perfil" value="${usuario.foto_perfil}" required />
+  
+            <div class="confirmaciones">
+              <button type="button" class="volver">Volver</button>
+              <button type="submit">Guardar Cambios</button>
+            </div>
+          </form>
+        `;
+
+      popupEdicionUsuarios
+        .querySelector(".volver")
+        .addEventListener("click", () => {
+          popupEdicionUsuarios.classList.remove("activo");
+          popupEdicionUsuarios.innerHTML = "";
+        });
+
+      popupEdicionUsuarios
+        .querySelector("#form-editar-usuario")
+        .addEventListener("submit", async (e) => {
+          e.preventDefault();
+
+          const nuevoNombre = e.target.nombre.value;
+          const nuevoEmail = e.target.email.value;
+          const nuevaFoto = e.target.foto_perfil.value;
+
+          try {
+            const respuesta = await fetch(`${usuariosURL}/editar-usuario`, {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+              body: JSON.stringify({
+                id_usuario: usuario.id_usuario,
+                nombre: nuevoNombre,
+                email: nuevoEmail,
+                foto_perfil: nuevaFoto,
+              }),
+            });
+
+            if (respuesta.ok) {
+              popupEdicionUsuarios.innerHTML = `<p>Usuario actualizado con éxito</p>`;
+              obtenerUsuarios();
+            } else {
+              popupEdicionUsuarios.innerHTML = `<p>Error actualizando usuario</p>`;
+            }
+          } catch (error) {
+            popupEdicionUsuarios.innerHTML = `<p>Error actualizando usuario</p>`;
+          } finally {
+            setTimeout(() => {
+              popupEdicionUsuarios.classList.remove("activo");
+            }, 1500);
+          }
+        });
+    });
+  });
+};
+
+popupUsuarios.addEventListener("click", async (e) => {
+  const token = localStorage.getItem("token");
+
+  if (e.target.classList.contains("volver")) {
+    popupUsuarios.classList.remove("activo");
+    return;
+  }
+
+  if (e.target.classList.contains("confirmar-eliminar")) {
+    const uid = e.target.dataset.id;
+    try {
+      const respuesta = await fetch(`${usuariosURL}/eliminar-usuario`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ id_usuario: uid }),
+      });
+      if (respuesta.ok) {
+        popupUsuarios.innerHTML = `<h2>Usuario eliminado con éxito</h2>`;
+        usuarios = usuarios.filter((e) => e.id_usuario !== parseInt(uid));
+        mostrarUsuarios(usuarios);
+        setTimeout(() => {
+          popupUsuarios.classList.remove("activo");
+        }, 1500);
+      } else {
+        console.error("Error al eliminar emoción");
+      }
+    } catch (error) {
+      console.error("Error en la petición de eliminación:", error);
+    }
+  }
+});
+
+document.getElementById("btn-nuevo-usuario").addEventListener("click", () => {
+  window.location = "register.html";
+});
+
+obtenerUsuarios();
